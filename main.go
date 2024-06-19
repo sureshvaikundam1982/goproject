2cc875ebc5876993b1013d98ff96fa350df87f3f
Suresh Vaikuntam <sureshvmsec@gmail.com>
2024-06-19 11:53:39 +0530

    After changes

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+type favNum int
+type favWord string
+type favBool bool
+
 func main() {
 	a := 2
 	b := 3
 	c := a + b
 	fmt.Println("Hello, World!", c)
+
+	var num favNum = 24
+	var word favWord = "take care health"
+	var boo favBool = true
+
+	fmt.Println("Hello", num)
+	fmt.Println("Hello", word)
+	fmt.Println("Hello", boo)
 
 }
