c7211d02ef47813e6d5186ee224db9913e14debe
Suresh Vaikuntam <sureshvmsec@gmail.com>
2024-06-19 12:12:26 +0530

    After changes on 6/19/2024 golang learning

--- main.go
+++ main.go
@@ -16,6 +16,28 @@ func main() {
 	var word favWord = "take care health"
 	var boo favBool = true
 
+	//var keyword
+	var empty string
+	var order int
+	var drink string = "milkshake"
+
+	//shorthand method
+	quantity := 1
+	food := "burrito"
+
+	// const
+	const pi = 3.14
+
+	fmt.Println("%T/n", empty)
+	fmt.Println("%T/n", order)
+	fmt.Println("%v/n", drink)
+	fmt.Println("%v/n", quantity)
+	fmt.Println("%T/n", quantity)
+	fmt.Println("%v/n", food)
+	fmt.Println("%T/n", food)
+	fmt.Println("%v/n", pi)
+	fmt.Println("%T/n", pi)
+
 	fmt.Println("Hello", num)
 	fmt.Println("Hello", word)
 	fmt.Println("Hello", boo)
